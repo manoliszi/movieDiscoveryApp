1f916dea010dcaedffed15c16d4049ae9cde7239
manolisziCy <manoliszi@cytech.gr>
2025-09-20 23:46:22 +0300

    initial commit. project setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <v-app>
+    <v-main class="d-flex align-center justify-center" style="height: 100vh;">
+      <v-btn color="primary">Hello Vuetify</v-btn>
+    </v-main>
+  </v-app>
+</template>
